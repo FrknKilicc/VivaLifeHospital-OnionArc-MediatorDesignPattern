2dee6e1c0fafc6451c6f954affab8941327b99d8
Furkan Kılıç <youkali40@gmail.com>
2024-01-25 22:22:17 +0300

    Updated Testimonial Api Consume

---
+++ Frontends/VivaLifeHospital.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialViewComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using VivaLifeHospital.Dto.TestimonialDtos;
+
+namespace VivaLifeHospital.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialViewComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialViewComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client =  _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7226/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+    }
+}
